ddabe678fd46ec9a2349657b2f58e98acb70939b
mnene-william <mnenewilliam26@gmail.com>
2025-09-15 12:33:54 +0300

    create a lesson model in models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,5 +9,19 @@ class Skill(models.Model):
     def __str__(self):
         return self.name
     
+class Lesson(models.Model):
+    category = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name="lessons")
+    title = models.CharField(max_length=200)
+    content = models.CharField(blank=True)
+    video = models.FileField(upload_to='lessons/videos/', blank=True, null=True)
+    video_url =models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.category.name} - {self.title}"
+    
+
+class Qu
+    
 
 
