26954d3a0ec9be43449d6b6bff341f658ee64a64
mnene-william <mnenewilliam26@gmail.com>
2025-09-15 12:39:18 +0300

    create a question model in models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -21,7 +21,20 @@ class Lesson(models.Model):
         return f"{self.category.name} - {self.title}"
     
 
-class Qu
+class QuizQuestion(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name="questions")
+    question_text = models.TextField()
+    option_a = models.CharField(max_length=200)
+    option_b = models.CharField(max_length=200)
+    option_c = models.CharField(max_length=200)
+    option_d = models.CharField(max_length=200)
+    correct_option = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.lesson.title} - {self.question_text[:200]}"
+    
+    
+    
     
 
 
