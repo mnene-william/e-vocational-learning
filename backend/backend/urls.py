200302ff969bcd54ad3d8605889653bc76a73a6a
mnene-william <mnenewilliam26@gmail.com>
2025-09-23 17:27:06 +0300

    add django_rest_passwordreset in installed apps inside settings.py and add the url endpoints in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path('api/password_reset/', include('django_rest_passwordreset.urls')),
     path("api/", include("api.urls")),
     
 ]
