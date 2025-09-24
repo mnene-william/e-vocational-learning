d3aee48421fa1934567081a54896721d058031ac
mnene-william <mnenewilliam26@gmail.com>
2025-09-24 11:58:21 +0300

    Add a background image in the contact.jsx component

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -8,9 +8,11 @@ function Contact(){
 
       <div className="min-h-screen bg-gray-50 text-gray-800">
 
-        <section className="bg-gradient-to-r from-blue-600 to-indigo-600 text-white py-16 text-center">
-            
+        <section className="relative bg-center bg-cover text-white py-24 text-center" style={{backgroundImage:"url('https://images.unsplash.com/photo-1521791055366-0d553872125f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80')"}}>
 
+        
+    
+           
         </section>
 
 
