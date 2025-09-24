6a4c72d0610e87ecf7238987465af65218a645ca
mnene-william <mnenewilliam26@gmail.com>
2025-09-24 12:01:50 +0300

    Add a black overlay on the background image in contact.jsx component

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -10,8 +10,7 @@ function Contact(){
 
         <section className="relative bg-center bg-cover text-white py-24 text-center" style={{backgroundImage:"url('https://images.unsplash.com/photo-1521791055366-0d553872125f?ixlib=rb-4.0.3&auto=format&fit=crop&w=1350&q=80')"}}>
 
-        
-    
+          <div className="absolute inset-0 bg-black bg-opacity-60"></div>
            
         </section>
 
