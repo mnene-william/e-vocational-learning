6fe106fa9201f73348b975e9b7ddda6bd0a9f753
mnene-william <mnenewilliam26@gmail.com>
2025-09-25 13:54:11 +0300

    Create UserProfile.jsx component and import in App.jsx and create a route for it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import HomePage from './components/HomePage';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import Contact from './components/Contact';
+import UserProfile from './components/UserProfile';
 
 
 import './App.css'
@@ -44,6 +45,8 @@ function App() {
           <Route path="/userprogress" element={<ProtectedRoute><UserProgress /></ProtectedRoute>} />
 
           <Route path="/contact" element={<Contact />} />
+
+          <Route path="/profile" element={<ProtectedRoute><UserProfile/></ProtectedRoute>}
 
           
 
