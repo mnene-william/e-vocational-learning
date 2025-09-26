b038095162aed0816912e3fae290a2df19427803
mnene-william <mnenewilliam26@gmail.com>
2025-09-26 04:07:15 +0300

    add an id field in LessonProgressSerializer in serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -90,13 +90,13 @@ class LessonProgressSerializer(serializers.ModelSerializer):
 
       class Meta:
           model = LessonProgress
-          fields = ["lesson", "lesson_title", "completed", "progress_percentage", "last_opened"]
+          fields = ["id", "lesson", "lesson_title", "completed", "progress_percentage", "last_opened"]
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username", read_only=True)
     email = serializers.EmailField(source="user.email", read_only=True)
-    progress = LessonProgressSerializer(source="user.progress", many=True, read_only=True)
+    progress = LessonProgressSerializer(many=True, read_only=True)
 
     class Meta:
         model = UserProfile
