73bd400347a696e6942f0ea1ef8ab2a54d9a209b
mnene-william <mnenewilliam26@gmail.com>
2025-09-29 12:40:20 +0300

    write a function that checks if user is admin/staff

--- backend/api/admin_views.py
+++ backend/api/admin_views.py
@@ -3,4 +3,14 @@ from rest_framework import viewsets, permissions
 from .models import *
 from .serializers import *
 
+class AdminOnly(permissions.BasePermission):
+      def has_permision(self, request, view):
+            return request.user and request.user.is_staff
+      
+
+
+      
+
+
+
 
