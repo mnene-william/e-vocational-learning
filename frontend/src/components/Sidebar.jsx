315ad2490b0f6b0ebb363f6813f0022118561f20
mnene-william <mnenewilliam26@gmail.com>
2025-09-29 14:26:42 +0300

    create a list of links in Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,27 @@
+import React from "react";
+import { Link, useLocation} from "react-router-dom"
 
 
 function Sidebar(){
+
+    const location = useLocation()
+
+    const links = [
+
+        {path:"/admin/dashboard", label:"Dashboard"},
+        {path:"/admin/users", label:"Users"},
+        {path:"/admin/lessons", label:"Lessons"},
+        {path:"/admin/quizzes", label:"Quizzes"},
+        {path:"/admin/reviews", label:"Reviews"},
+        {path:"/admin/progress", label:"Progress"},
+
+    ]
+
+    return(<>
+    
+    
+    
+    </>)
 
 
 }
