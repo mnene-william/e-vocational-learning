e81138b7dfd2e8734e20a2f047376e7d26db8448
mnene-william <mnenewilliam26@gmail.com>
2025-09-29 13:18:27 +0300

    create routers and paths in admin_urls.py

--- backend/api/admin_urls.py
+++ backend/api/admin_urls.py
@@ -1,5 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .admin_views import *
 
 
 router = DefaultRouter()
+
+router.register(r"users", AdminUserViewSet, basename="admin-users")
+router.register(r"lessons", AdminLessonViewSet, basename="admin-lessons")
+router.register(r"quizzes", AdminQuizViewSet, basename="admin-reviews")
+router.register(r"profiles", AdminUserProfileViewSet, basename="admin-profiles")
+router.register(r"reviews", AdminReviewViewSet, basename="admin-reviews")
+
+urlpatterns = [
+    path('',include(router.urls)),
+]
